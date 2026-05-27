86906a1791be69c594d5ac3f64af81e6ec009202
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2026-05-27 13:05:55 -0300

    Initial commit

---
+++ backend/src/main/java/com/example/figuritas/FiguritasApplication.java
@@ -0,0 +1,13 @@
+package com.example.figuritas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiguritasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiguritasApplication.class, args);
+	}
+
+}
