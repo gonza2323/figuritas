ee9b211b7c936636be23f8178be7a67bfc7d7f32
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2026-05-28 14:59:45 -0300

    Add initial avatar images

--- backend/fetch_random_avatars.py
+++ backend/fetch_random_avatars.py
@@ -2,13 +2,13 @@ import requests
 from pathlib import Path
 
 AVATARS_PATH = "src/main/resources/users/avatars"
-
+NO_AVATARS = 20
 
 Path(AVATARS_PATH).mkdir(exist_ok=True)
 
-for i in range(1, 201):
-    url = f"https://api.dicebear.com/9.x/adventurer/png?seed=user{i}"
+for i in range(0, NO_AVATARS):
+    url = f"https://api.dicebear.com/9.x/adventurer/png?seed={i}"
     img = requests.get(url).content
 
-    with open(f"{PATH}/user{i}.png", "wb") as f:
+    with open(f"{AVATARS_PATH}/avatar_{i:02d}.png", "wb") as f:
         f.write(img)
