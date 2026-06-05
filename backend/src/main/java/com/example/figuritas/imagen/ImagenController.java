4eb58e03763d9edf23b27482f6963e07de5c4407
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2026-06-05 14:23:41 -0300

    Add Image caching

--- backend/src/main/java/com/example/figuritas/imagen/ImagenController.java
+++ backend/src/main/java/com/example/figuritas/imagen/ImagenController.java
@@ -2,6 +2,7 @@ package com.example.figuritas.imagen;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/imagenes")
@@ -20,9 +23,14 @@ public class ImagenController {
     public ResponseEntity<byte[]> getImagen(@PathVariable Long imagenId) {
         ImagenViewDto imagen = service.findDto(imagenId);
 
+        CacheControl cacheControl = CacheControl
+                .maxAge(24, TimeUnit.HOURS)
+                .cachePublic();
+
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + imagen.getNombre() + "\"")
                 .contentType(MediaType.parseMediaType(imagen.getMime()))
+                .cacheControl(cacheControl)
                 .body(imagen.getContenido());
     }
 }
